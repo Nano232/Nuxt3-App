679afc331d7a9e7f766d544b48389e0fafaf2e2f
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2023-10-14 14:09:57 +0300

    update

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <p>{{ products.title }}</p>
-    <p>{{ products.price }}</p>
-    <p>{{ products.id }}</p>
+  <div class="card">
+    <div class="grid grid-cols-2 gap-10">
+      <div class="p-7">
+        <img :src="product.image" alt="image" class="mx-auto my-7" />
+      </div>
+      <div>
+        <h2 class="text-4xl my-7">{{ product.title }}</h2>
+        <p class="text-xl my-7">Price - ${{ product.price }}</p>
+        <h3 class="font-bold border-b-2 mb-4 pb-2">Product Description</h3>
+        <p class="mb-7">{{ product.description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +18,7 @@
 const { id } = useRoute().params;
 const url = "https://fakestoreapi.com/products/" + id;
 
-const { data: products } = await useFetch(url, { key: id });
+const { data: product } = await useFetch(url);
 
 definePageMeta({
   layout: "products",

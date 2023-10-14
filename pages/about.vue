530d00289e5c09ff3887ab285a19c15b59d6620f
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2023-10-14 10:52:24 +0300

    update

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>About Mohamed Elzorkany</h1>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+      officia fugiat voluptate aspernatur possimus suscipit laborum explicabo.
+      Id, qui itaque?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+      officia fugiat voluptate aspernatur possimus suscipit laborum explicabo.
+      Id, qui itaque?
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>

afcb82b33fb7da3603572a7166b40ecc88f98c49
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2023-10-14 10:54:26 +0300

    update

---
+++ pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Index</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+      officia fugiat voluptate aspernatur possimus suscipit laborum explicabo.
+      Id, qui itaque?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+      officia fugiat voluptate aspernatur possimus suscipit laborum explicabo.
+      Id, qui itaque?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
